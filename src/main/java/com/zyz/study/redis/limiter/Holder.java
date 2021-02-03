97689bb12bd9bb7b5df4eedc8bb6f293356d7755
zhiling <yunzhuang.zyz@alibaba-inc.com>
2021-02-03 11:42:32 +0800

    redis limiter

---
+++ src/main/java/com/zyz/study/redis/limiter/Holder.java
@@ -0,0 +1,13 @@
+package com.zyz.study.redis.limiter;
+
+import lombok.Data;
+
+/**
+ * @author 执零
+ * @version 1.0
+ * @date 2021/1/28 10:03
+ */
+@Data
+public class Holder<T> {
+    private T val;
+}
--- src/main/java/com/zyz/study/redis/limiter/SimpleLimiter.java
+++ src/main/java/com/zyz/study/redis/limiter/SimpleLimiter.java
@@ -1,6 +1,8 @@
 package com.zyz.study.redis.limiter;
 
+import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.omg.CORBA.LongHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
@@ -9,8 +11,12 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
+ * 基于时间窗口实现的限流算法
+ * 缺点：对于60s内限制访问100w次这种场景，不能很好的支持，太占内存了
+ * 这个问题可以通过漏斗算法解决
  * @author 执零
  * @version 1.0
  * @date 2021/1/27 17:55
@@ -26,24 +32,23 @@ public class SimpleLimiter implements Limiter {
     public boolean pass(Long userId, String action, Long timeWindowSeconds, Integer limitCount) {
         long now = System.currentTimeMillis();
         byte[] key = String.format("limit:%s:%s", userId, action).getBytes(StandardCharsets.UTF_8);
-
-
-
-        redisTemplate.executePipelined((RedisCallback<Object>) redisConnection -> {
+        List<List> list = redisTemplate.executePipelined((RedisCallback<Object>) redisConnection -> {
+            // 开启事务，防止保证指令连续执行 类似begin
             redisConnection.multi();
             // 存入redis zadd key score value
             redisConnection.zAdd(key, now, String.valueOf(now).getBytes(StandardCharsets.UTF_8));
-            // 时间窗口之外的直接删除
             long min = now - timeWindowSeconds * 1000;
+            // 时间窗口之外的直接删除 zremrangebyscore key min max
             redisConnection.zRemRangeByScore(key, 0, min);
-            // 查询出当前剩下的记录
-            Long curCount;
-            curCount = redisConnection.zCard(key);
+            // 查询出当前剩下的记录 zcard key
+            Long curCount = redisConnection.zCard(key);
             // 重置过期时间，这里的参数是秒,多宽限一秒
             redisConnection.expire(key, timeWindowSeconds + 1);
+            // 类似commit
             redisConnection.exec();
             return null;
         });
-        return true;
+        log.info(JSONUtil.toJsonStr(list));
+        return ((long)list.get(0).get(2)) <= limitCount;
     }
 }
